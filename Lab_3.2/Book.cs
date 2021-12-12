182968f99217b3561aee946b191bcad8a50b5135
voloshkin-v <56509285+voloshkin-v@users.noreply.github.com>
2021-12-12 13:32:37 +0200

    Add files via upload

---
+++ Lab_3.2/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_2
+{
+    public class Book
+    {
+        public long SerialNumber { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+        public int SamplesCount { get; set; }
+
+        public Book() { }
+        
+        public Book(long serialNumber, string name, int year, double price, int samplesCount)
+        {
+            SerialNumber = serialNumber;
+            Name = name;
+            Year = year;
+            Price = price;
+            SamplesCount = samplesCount;
+        }
+
+        public void IncreasePrice(double percentage)
+        {
+            Price = Price + Price / 100 * percentage;
+        }
+
+        public double EditionPrice => Price * SamplesCount;
+
+        public override string ToString() => $"{SerialNumber} {Name} {Price}";
+    }
+}
---
+++ Lab_3.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Book(1, "Book1", 2001, 100, 12);
+            var book2 = new Book(2, "Book2", 2011, 50, 10);
+            var book3 = new Book(3, "Book3", 2020, 70, 5);
+            var book4 = new Book(4, "Book4", 2005, 200, 2);
+
+            // Array
+            var array = new[] {book1, book2, book3, book4};
+
+            // Generic
+            var list = new List<Book>();
+            list.Add(book1);
+            list.Add(book2);
+            list.Add(book3);
+            list.Add(book4);
+
+            list.Remove(book1);
+            list.Remove(book4);
+
+            // Non-generic
+            var nonGenericList = new List<object>();
+            nonGenericList.Add(book1);
+            nonGenericList.Add(book2);
+            nonGenericList.Add(book3);
+            nonGenericList.Add(book4);
+
+            nonGenericList.Remove(book1);
+            nonGenericList.Remove(book4);
+
+            Console.WriteLine("Foreach in array");
+            foreach (var book in array)
+                Console.WriteLine(book);
+
+            Console.WriteLine();
+            
+            Console.WriteLine("Foreach in list");
+            foreach (var book in list)
+                Console.WriteLine(book);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Foreach in nonGenericList");
+            foreach (var book in nonGenericList)
+                Console.WriteLine(book as Book);
+        }
+    }
+}
